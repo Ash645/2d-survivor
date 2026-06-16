40963d3dca8c71c0acefe2a6b80e5329528a37df
Ashwin Karthikeyan <ashwinkarthik645@gmail.com>
2026-06-16 21:26:03 +0100

    added gitignore

--- .godot/mono/temp/obj/Debug/2DSurvivor.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/2DSurvivor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2DSurvivor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4177b8c824c4214b24bcdb846d84e1fc4961cbaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("2DSurvivor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2DSurvivor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
